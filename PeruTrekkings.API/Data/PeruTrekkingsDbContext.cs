306342af23134645daa447d399ed1f2080a4249c
OrlandoYV <sergioyv@gmail.com>
2024-05-16 12:10:33 -0500

    oy - connection string

---
+++ PeruTrekkings.API/Data/PeruTrekkingsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeruTrekkings.API.Models.Domain;
+
+namespace PeruTrekkings.API.Data
+{
+    public class PeruTrekkingsDbContext : DbContext
+    {
+        public PeruTrekkingsDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+
+        }
+
+        public DbSet<Difficulty>   Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+
+    }
+}
