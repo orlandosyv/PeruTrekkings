2f073a5f2a3aeb505a887b8d0be1329175b5b54e
OrlandoYV <sergioyv@gmail.com>
2024-05-29 15:41:22 -0500

    oy - getting data from API - regions

---
+++ PeruTrekkings.UI/Controllers/RegionsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeruTrekkings.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                //Get all Regions from WebAPI
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7153/api/regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode(); //check status
+
+                var stringResponseBody =  await httpResponseMessage.Content.ReadAsStringAsync();
+
+                ViewBag.Response = stringResponseBody;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+            return View();
+        }
+    }
+}
